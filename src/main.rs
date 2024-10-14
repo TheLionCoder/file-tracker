81a7c530c1e794d82d5436c873eb6d3fdb054885
TheLionCoder <eorlando.reyes@icloud.com>
2024-10-14 11:04:27 -0500

    finish logic

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 use std::error::Error;
-use std::path::PathBuf;
-use std::sync::Mutex;
+use std::path::{Path, PathBuf};
+use std::sync::{Arc, Mutex, MutexGuard};
 
 use clap::ArgMatches;
 use rayon::prelude::*;
@@ -26,26 +26,53 @@ fn main() -> Result<(), Box<dyn Error>> {
         .parse()
         .unwrap();
 
-    let inventory: Mutex<HashMap<String, usize>> = Mutex::new(HashMap::new());
+    let inventory: Arc<Mutex<HashMap<String, usize>>> = Arc::new(Mutex::new(HashMap::new()));
 
     WalkDir::new(dir_path)
-        .max_depth(0)
         .max_depth(max_depth)
         .into_iter()
         .filter_entry(|entry| !is_hidden(entry))
         .par_bridge()
         .filter_map(Result::ok)
         .filter(|entry| entry.file_type().is_file())
+        .filter(|entry| is_text_or_csv_file(entry.path()))
         .for_each(|entry| {
             let path: PathBuf = entry.path().to_path_buf();
+            let inventory: Arc<Mutex<HashMap<String, usize>>> = Arc::clone(&inventory);
             if let Ok(line_count) = count_lines(&path, header) {
                 // use the lock method to access the inventory
                 // in a thread-safe manner
-                let mut inventory = inventory.lock().unwrap();
+                let mut inventory: MutexGuard<HashMap<String, usize>> = inventory.lock().unwrap();
                 inventory.insert(path.to_str().unwrap().to_string(), line_count);
             }
         });
 
     parse_inventory::write_inventory(inventory, dir_path)?;
     Ok(())
+}
+
+fn is_text_or_csv_file(path: &Path) -> bool {
+    matches!(
+        path.extension().and_then(|ext| ext.to_str())
+        .map(|ext| ext.to_ascii_lowercase()),
+        Some(ref ext)  if ext == "txt" || ext == "csv"
+    )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_is_text_or_csv_file() {
+        let txt_file: &Path = Path::new("test.txt");
+        let upper_txt_file: &Path = Path::new("test.TXT");
+        let csv_file: &Path = Path::new("test.csv");
+        let other_file: &Path = Path::new("test,xlsx");
+
+        assert!(is_text_or_csv_file(txt_file));
+        assert!(is_text_or_csv_file(upper_txt_file));
+        assert!(is_text_or_csv_file(csv_file));
+        assert!(!is_text_or_csv_file(other_file));
+    }
 }
--- src/parse_inventory.rs
+++ src/parse_inventory.rs
@@ -1,11 +1,11 @@
 use std::collections::HashMap;
 use std::path::PathBuf;
-use std::sync::Mutex;
+use std::sync::{Arc, Mutex};
 
-use rust_xlsxwriter::{Format, Formula, Workbook, Worksheet, XlsxError};
+use rust_xlsxwriter::{Color, Format, FormatAlign, FormatBorder, Formula, Workbook, Worksheet, XlsxError};
 
 pub(crate) fn write_inventory(
-    inventory: Mutex<HashMap<String, usize>>,
+    inventory: Arc<Mutex<HashMap<String, usize>>>,
     path: &str,
 ) -> Result<(), XlsxError> {
     // Lock the inventory to print the results
@@ -13,20 +13,46 @@ pub(crate) fn write_inventory(
     let mut row: u32 = 1_u32;
 
     let mut workbook = Workbook::new();
-    let mut worksheet: Worksheet = Worksheet::new();
+    let  worksheet: &mut Worksheet = workbook.add_worksheet().set_name("inventory")?;
 
-    let bold: Format = Format::new().set_bold();
-    let number_format: Format = Format::new().set_num_format("#, #####");
+    let header: Format = Format::new()
+        .set_bold()
+        .set_align(FormatAlign::Center)
+        .set_border(FormatBorder::DashDot)
+        .set_background_color(Color::Navy)
+        .set_font_color(Color::White);
 
-    worksheet.write_with_format(0, 0, "directory", &bold)?;
-    worksheet.write_with_format(0, 1, "sub-directory", &bold)?;
-    worksheet.write_with_format(0, 2, "file", &bold)?;
-    worksheet.write_with_format(0, 3, "line_count", &bold)?;
+    let total: Format = Format::new()
+        .set_bold()
+        .set_italic()
+        .set_border(FormatBorder::Double)
+        .set_background_color(Color::Yellow);
+
+    let data_format: Format = Format::new()
+        .set_align(FormatAlign::Center)
+        .set_border(FormatBorder::Thin);
+
+    let number_format: Format = Format::new()
+        .set_align(FormatAlign::Center)
+        .set_border(FormatBorder::Thin)
+        .set_num_format("#, #####");
+
+    worksheet.set_tab_color(Color::Blue);
+    worksheet.autofilter(0, 0, 0, 3)?;
+
+    worksheet.write_with_format(0, 0, "directory", &header)?;
+    worksheet.write_with_format(0, 1, "sub-directory", &header)?;
+    worksheet.write_with_format(0, 2, "file", &header)?;
+    worksheet.write_with_format(0, 3, "line_count", &header)?;
 
     for (path, line_count) in &*inventory {
         let path: PathBuf = PathBuf::from(path);
         let file_name: &str = path.file_name().unwrap().to_str().unwrap();
-        let parent: &str = path.parent().unwrap().to_str().unwrap();
+        let parent: &str = path.components()
+            .next()
+            .unwrap()
+            .as_os_str()
+            .to_str().unwrap();
         let ancestor_component: &str = path
             .components()
             .nth(1)
@@ -35,20 +61,21 @@ pub(crate) fn write_inventory(
             .to_str()
             .unwrap();
 
-        worksheet.write(row, 0, parent)?;
-        worksheet.write(row, 1, ancestor_component)?;
-        worksheet.write(row, 2, file_name)?;
-        worksheet.write(row, 3, *line_count as u32)?;
+        worksheet.write_with_format(row, 0, parent, &data_format)?;
+        worksheet.write_with_format(row, 1, ancestor_component, &data_format)?;
+        worksheet.write_with_format(row, 2, file_name, &data_format)?;
+        worksheet.write_with_format(row, 3, *line_count as u32, &number_format)?;
         row += 1;
     }
-    worksheet.write_with_format(row, 2, "TOTAL", &bold)?;
+    worksheet.write_with_format(row, 2, "TOTAL", &total)?;
     worksheet.write_with_format(
         row,
         3,
-        Formula::new(format!("SUM(D1:D{}", row)),
-        &number_format,
+        Formula::new(format!("SUM(D1:D{})", row)),
+        &total
     )?;
 
+    worksheet.autofit();
     workbook.save(PathBuf::from(path).join("inventory.xlsx"))?;
     Ok(())
 }
