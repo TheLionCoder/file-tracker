445b1022b0153f89d98dfc3f09010419d37d8f5b
TheLionCoder <eorlando.reyes@icloud.com>
2024-12-14 16:33:12 -0500

    add tracing for logging in console

--- src/inventory_processor.rs
+++ src/inventory_processor.rs
@@ -1,11 +1,4 @@
-use std::collections::HashMap;
-use std::path::{Path, PathBuf};
-use std::sync::{Arc, Mutex, MutexGuard};
-
-use rayon::prelude::*;
-use walkdir::WalkDir;
-
-use crate::file_len_inventory::{count_lines, is_hidden};
+use std::path::{Path};
 
 enum FileExtension {
     Txt,
@@ -23,36 +16,7 @@ impl FileExtension {
     }
 }
 
-pub(crate) fn process_inventory(
-    dir_path: &Path,
-    max_depth: usize,
-    header: bool,
-) -> Arc<Mutex<HashMap<PathBuf, usize>>> {
-    let inventory: Arc<Mutex<HashMap<PathBuf, usize>>> = Arc::new(Mutex::new(HashMap::new()));
-
-    WalkDir::new(dir_path)
-        .max_depth(max_depth)
-        .into_iter()
-        .filter_entry(|entry| !is_hidden(entry))
-        .par_bridge()
-        .filter_map(Result::ok)
-        .filter(|entry| entry.file_type().is_file())
-        .filter(|entry| is_text_or_csv_file(entry.path()))
-        .for_each(|entry| {
-            let path: PathBuf = entry.path().to_path_buf();
-            let inventory: Arc<Mutex<HashMap<PathBuf, usize>>> = Arc::clone(&inventory);
-
-            if let Ok(line_count) = count_lines(&path, header) {
-                // use the lock method to access the inventory
-                // in a thread-safe manner
-                let mut inventory: MutexGuard<HashMap<PathBuf, usize>> = inventory.lock().unwrap();
-                inventory.insert(path, line_count);
-            }
-        });
-    inventory
-}
-
-fn is_text_or_csv_file(path: &Path) -> bool {
+pub(crate) fn is_text_or_csv_file(path: &Path) -> bool {
     matches!(
         FileExtension::from_path(path),
         FileExtension::Txt | FileExtension::Csv
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
+use std::collections::HashMap;
 use clap::ArgMatches;
-use inventory_processor::process_inventory;
 use parse_inventory::write_inventory;
 use std::error::Error;
-use std::path::Path;
+use std::path::{Path, PathBuf};
+use std::sync::{Arc, Mutex, MutexGuard};
+use rayon::prelude::*;
+use tracing::{event, span, Level, Span};
+use walkdir::WalkDir;
+
+use crate::file_len_inventory::{count_lines, is_hidden};
 
 mod cli_parsing;
 mod file_len_inventory;
@@ -10,6 +16,8 @@ mod inventory_processor;
 mod parse_inventory;
 
 fn main() -> Result<(), Box<dyn Error>> {
+    tracing_subscriber::fmt::init();
+
     let matches: ArgMatches = cli_parsing::parse_cli();
     let dir_path_str: &str = matches
         .get_one::<String>("dir-path")
@@ -23,8 +31,35 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let dir_path: &Path = Path::new(dir_path_str);
 
-    let inventory = process_inventory(dir_path, max_depth, header);
+    let inventory: Arc<Mutex<HashMap<PathBuf, usize>>> = Arc::new(Mutex::new(HashMap::new()));
 
+    event!(Level::INFO, "Starting inventory processing");
+    WalkDir::new(dir_path)
+        .max_depth(max_depth)
+        .into_iter()
+        .filter_entry(|entry| !is_hidden(entry))
+        .par_bridge()
+        .filter_map(Result::ok)
+        .filter(|entry| entry.file_type().is_file())
+        .filter(|entry| inventory_processor::is_text_or_csv_file(entry.path()))
+        .for_each(|entry| {
+            let span: Span = span!(Level::INFO, "Calculating line count");
+            let _guard = span.enter();
+
+            let path: PathBuf = entry.path().to_path_buf();
+            let file_name: &str = path.file_stem().unwrap().to_str().unwrap();
+
+            event!(Level::INFO, "Calculatin line count for {}", file_name);
+            let inventory: Arc<Mutex<HashMap<PathBuf, usize>>> = Arc::clone(&inventory);
+            if let Ok(line_count) = count_lines(&path, header) {
+                // use the lock method to access the inventory
+                // in a thread-safe manner
+                let mut inventory: MutexGuard<HashMap<PathBuf, usize>> = inventory.lock().unwrap();
+                inventory.insert(path, line_count);
+            }
+        });
+    event!(Level::INFO, "Writing inventory to xlsx file");
     write_inventory(inventory, dir_path)?;
+    event!(Level::INFO, "Inventory processing complete");
     Ok(())
 }
